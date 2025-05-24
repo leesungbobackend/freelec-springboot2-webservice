6ed99dd012c8c3809f2870cba755a0815056cbb5
leesungbobackend <leesungbobackend@gmail.com>
2025-05-24 21:17:38 +0900

    first commit

---
+++ src/main/java/com/leesungbo/book/springboot/web/dto/PostResponseDto.java
@@ -0,0 +1,20 @@
+package com.leesungbo.book.springboot.web.dto;
+
+import com.leesungbo.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
